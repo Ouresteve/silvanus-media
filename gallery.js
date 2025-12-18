439c6fa936ad065cc40885812d062a111b503f17
Steve <steveoure96@gmail.com>
2025-12-18 16:36:40 +0300

    latest update

--- gallery.js
+++ gallery.js
@@ -6,27 +6,35 @@ const images = [
   "Images/1765973581410.jpg",
   "Images/1765973616230.jpg"
 ];
-const grid= document.getElementById("thumbGrid");
+const galleryGrid= document.getElementById("galleryGrid");
+const viewer = document.getElementById("viewer");
+const viewerImg= document.getElementById("viewerImg");
+const viewerClose = document.getElementById("viewerClose");
 
-const lightbox=document.getElementById("lightbox");
-const lightboxImg=document.getElementById("lightboxImg");
-const closeBtn= document.getElementById("closeBtn");
+images.forEach(src=>{
+    const thumb=document.createElement("div");
+    thumb.className="gallery-thumb";
 
-images.forEach(src=> {
-    const img=document.createElement("img");
+    const img =document.createElement("img");
     img.src=src;
-    img.onclick = () => {
-        lightbox.style.display="flex";
-        lightboxImg.src=src;
-    };
-    grid.appendChild(img);
-});
-closeBtn.onclick= () => {
-    lightbox.style.display="none";
-};
 
-lightbox.onclick=e=>{
-        if (e.target===lightbox){
-            lightbox.style.display="none";
-        }
-};
+    img.addEventListener("click",()=> {
+        viewer.style.display="block";
+        viewerImg.src=src;
+    
+
+    });
+
+    thumb.appendChild(img);
+    galleryGrid.appendChild(thumb);
+});
+
+viewerClose.addEventListener("click",()=> {
+    viewer.style.display="none";
+});
+
+viewer.addEventListener("click",e=> {
+    if(e.target === viewer) {
+        viewer.style.display="none";
+    }
+});
